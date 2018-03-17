7413f10e554bf1b462e349d3fcd3c83238c72c83
darkzero_omega <darkzero_omega2003@yahoo.com>
2018-03-17 09:51:33 -0400

    added more js and added framework for chat

---
+++ assets/javascript/app.js
@@ -0,0 +1,71 @@
+//!-- Players Score Holder --//
+const player1Score = 0;
+const player2Score = 0;
+const player1Score_span = document.getElementById("player1Score");
+const player2Score_span = document.getElementById("player2Score");
+const scoreBoard_div = document.querySelector(".score-board");
+const results_div = document.querySelector(".results");
+
+
+//!-- RPS Conts --//
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+
+    
+
+
+function game(playerr1Choice)
+
+
+
+function main() {
+    rock_div.addEventListener("click", function() {
+        game("r");
+
+
+})
+
+   paper_div.addEventListener("click", function() {
+        game("p");
+
+})
+
+   scissors_div.addEventListener("click", function() {
+        game("s");
+
+
+})
+
+};
+
+
+
+function game(player2Choice){
+    
+
+}
+
+
+function main() {
+    rock_div.addEventListener("click", function() {
+        game("r");
+
+
+})
+
+   paper_div.addEventListener("click", function() {
+        game("p");
+
+})
+
+   scissors_div.addEventListener("click", function() {
+        game("s");
+
+
+})
+
+};
+
+main();
---
+++ assets/javascript/client.js
@@ -0,0 +1,12 @@
+function onMessage(text){
+
+
+    var list = document.getElementById("chat");
+    var elem = document.createElement("li");
+    el.innerHTML = text;
+    list.appendChild(elem);
+}
+
+
+onMessage('Hi');
+onMessage('I am connected');
